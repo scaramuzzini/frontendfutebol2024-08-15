649f49201a7e7af38569c4d88a8ce4bd4721c850
Mario Torres <scaramuzzini@gmail.com>
2024-08-15 09:47:54 -0400

    cRud - Leitura de times

--- src/components/TimesList.js
+++ src/components/TimesList.js
@@ -1,5 +1,51 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const headers = {
+    "ngrok-skip-browser-warning": "123"
+}
+
 function TimesList() {
-    return <h2>Lista de times...</h2>
+
+    const [times,setTimes] = useState([]);
+
+    const fetchTimes = async () => {
+        const response = await axios
+            .get('https://146d-191-165-254-55.ngrok-free.app/times'
+                , {
+                    headers: headers
+                }
+            );
+        console.log(response.data);
+        setTimes(response.data);
+    }
+
+    useEffect(() => {
+        fetchTimes();
+    }, []);
+
+    return (
+        <>
+            <h2>Tabela de Times</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome do Time</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {times.map((time) => (
+                        <tr>
+                            <td>{time.id}</td>
+                            <td>{time.nome}</td>
+                        </tr>
+                    ))}
+                </tbody>
+                
+            </table>
+        </>
+    );
 }
 
 export default TimesList;
